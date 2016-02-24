7e2212ea4f18d3602f0bd60a3418e4887562d222
gaurav-sam <gauravsam34@gmail.com>
2016-02-24 21:46:37 +0530

    create todos and todos:id API

--- server.js
+++ server.js
@@ -9,3 +9,42 @@ app.get('/', function(req, res) {
 app.listen(PORT, function() {
 	console.log('app is working on port no 3000');
 });
+
+var todos = [{
+	id : 1,
+	description : 'task 1',
+	complete : false
+},{
+	id : 2,
+	description : 'task 2',
+	complete : false
+},{
+	id : 3,
+	description : 'task 3',
+	complete : true
+}];
+
+app.get('/todos', function(req, res) {
+	res.json(todos);
+})
+
+console.log(todos[0].id)
+app.get('/todos/:id', function (req, res) {
+	var matchId = parseInt(req.params.id, 10);
+	var matchedTodo;
+	for (var i = 0; i < todos.length; i++) {
+		if (matchId === todos[i].id) {
+			matchedTodo = todos[i];
+		} 
+	}
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+})
+
+// if (todos[i].id === matchId) {
+// 			res.send(todos[i]);
+// 		} 
