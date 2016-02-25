c3c904400992f48ee4dffd500ba9fd0bdc6244b6
gaurav-sam <gauravsam34@gmail.com>
2016-02-26 00:23:53 +0530

    changes on package json

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('gaurav')
@@ -10,41 +14,41 @@ app.listen(PORT, function() {
 	console.log('app is working on port no 3000');
 });
 
-var todos = [{
-	id : 1,
-	description : 'task 1',
-	complete : false
-},{
-	id : 2,
-	description : 'task 2',
-	complete : false
-},{
-	id : 3,
-	description : 'task 3',
-	complete : true
-}];
+var todos  = [];
+var todoId = 1;
 
 app.get('/todos', function(req, res) {
 	res.json(todos);
 })
 
-console.log(todos[0].id)
+// console.log(todos[0].id)
 app.get('/todos/:id', function (req, res) {
-	var matchId = parseInt(req.params.id, 10);
-	var matchedTodo;
-	for (var i = 0; i < todos.length; i++) {
-		if (matchId === todos[i].id) {
-			matchedTodo = todos[i];
-		} 
-	}
+	var todoId = parseInt(req.params.id, 10);
+
+	var matchedTodo = _.findWhere(todos, {id : todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
-})
+});
 
-// if (todos[i].id === matchId) {
+app.post('/todos', function(req, res) {
+	var body = req.body;
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		res.status(404).send();
+	}
+
+	body.id = todoId++;
+
+	todos.push(body);
+
+	res.json(body)
+
+});
+
+// if (todos[i].id === todoId) {
 // 			res.send(todos[i]);
-// 		} 
+// 		}
